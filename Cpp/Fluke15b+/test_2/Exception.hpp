d2538a84e77d34a44f62c449b2047ea02b7afa0b
reivax-boucoi <reivax.boucoi@laposte.net>
2018-10-21 15:54:25 +0200

    a

---
+++ Cpp/Fluke15b+/test_2/Exception.hpp
@@ -0,0 +1,38 @@
+
+#ifndef MN_CPP_LINUX_SERIAL_EXCEPTION_H_
+#define MN_CPP_LINUX_SERIAL_EXCEPTION_H_
+
+// System includes
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace mn {
+    namespace CppLinuxSerial {
+
+        class Exception : public std::runtime_error {
+
+        public:
+            Exception(const char *file, int line, const std::string &arg) :
+                    std::runtime_error(arg) {
+                msg_ = std::string(file) + ":" + std::to_string(line) + ": " + arg;
+            }
+
+            ~Exception() throw() {}
+
+            const char *what() const throw() override {
+                return msg_.c_str();
+            }
+
+        private:
+            std::string msg_;
+        };
+
+    } // namespace CppLinuxSerial
+} // namespace mn
+
+#define THROW_EXCEPT(arg) throw Exception(__FILE__, __LINE__, arg);
+
+
+#endif // MN_CPP_LINUX_SERIAL_EXCEPTION_H_
