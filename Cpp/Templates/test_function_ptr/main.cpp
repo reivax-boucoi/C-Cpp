3c697fbfcba82a2ed9336116e577484b0e227afa
reivax-boucoi <reivax.boucoi@laposte.net>
2018-11-03 12:42:28 +0100

    a

--- Cpp/Templates/test_function_ptr/main.cpp
+++ Cpp/Templates/test_function_ptr/main.cpp
@@ -8,13 +8,15 @@ void fun1(int i){
 void fun2(int i, int j){
  cout<<"fun 2 args "<<i<<" and "<<j<<endl;   
 }
+typedef void (*fn_ptr)(int);
 
-map<string , float> cmdLookUp;
+map<string , fn_ptr> cmdLookUp;
     
 
 int main(void){
-    cmdLookUp["test"]=5.15;
-    cmdLookUp["tes"]=15;
-    cout<<"hi"<<cmdLookUp["test"]<<endl;
+    cmdLookUp["test"]=fun1;
+    cmdLookUp["test2"]=fun2;
+    cmdLookUp["test"](3);
+    cout<<"hi"<<endl;
     return 0;
 }
