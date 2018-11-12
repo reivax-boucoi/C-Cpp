8b4ee66e6fadd65b31418ad56cc9cc4f3004558e
reivax-boucoi <reivax.boucoi@laposte.net>
2018-11-12 12:40:09 +0100

    a

--- Cpp/Templates/test_function_ptr/main.cpp
+++ Cpp/Templates/test_function_ptr/main.cpp
@@ -2,6 +2,12 @@
 #include <map>
 using namespace std;
 
+void fun1(int i){
+ cout<<"fun 1 arg "<<i<<endl;   
+}
+void fun2(int i, int j){
+ cout<<"fun 2 args "<<i<<" and "<<j<<endl;   
+}
 
 map<string , float> cmdLookUp;
     
