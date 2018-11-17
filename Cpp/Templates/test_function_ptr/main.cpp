2068208ecffea873de0326d4940bb8bc8b45147d
reivax-boucoi <reivax.boucoi@laposte.net>
2018-11-17 12:52:27 +0100

    a

--- Cpp/Templates/test_function_ptr/main.cpp
+++ Cpp/Templates/test_function_ptr/main.cpp
@@ -2,13 +2,13 @@
 #include <map>
 using namespace std;
 
-void fun1(int i){
+void fun1(int i,...){
  cout<<"fun 1 arg "<<i<<endl;   
 }
-void fun2(int i, int j){
- cout<<"fun 2 args "<<i<<" and "<<j<<endl;   
+void fun2(int i...){
+ cout<<"fun 2 args "<<i<<" and "<<endl;   
 }
-typedef void (*fn_ptr)(int);
+typedef void (*fn_ptr)(int,...);
 
 map<string , fn_ptr> cmdLookUp;
     
