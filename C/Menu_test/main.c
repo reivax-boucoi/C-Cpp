3a92563b27f8d7e76bbc45c4b378eaf7220168a6
reivax-boucoi <reivax.boucoi@laposte.net>
2018-12-07 12:10:59 +0100

    a

--- C/Menu_test/main.c
+++ C/Menu_test/main.c
@@ -24,12 +24,30 @@ struct Led{
     {"1/2",0},
     {"VINGT",0}//18
 };
-void setHour(int i){
-    printf("Executed setHour with parameter %d\r\n",i);
-}
 
 void setMode(int i){
-    printf("Executed setMode with parameter %d\r\n",i);
+}
+void setColorMode(int i){
+}
+void setNightMode(int i){
+}
+void setNightOff(int i){
+}
+void setNightOn(int i){
+}
+void setHouram(int i){
+}
+void setHourpm(int i){
+}
+void setMinam(int i){
+}
+void setMinpm(int i){
+}
+void setEventMode(int i){
+}
+void reset(int i){
+}
+void setRainbow(int i){
 }
 struct Menu;
 typedef const struct Menu{
@@ -37,12 +55,35 @@ typedef const struct Menu{
     const char* color;
     const int nb_optn;
     void (*fptr)(int);
-    struct{
+    const struct{
         const int led;
         const struct Menu* submenu;
     }sub[];
 };
 struct Menu M0main;
+struct Menu M1mode;
+struct Menu M1cmode;
+struct Menu M1nightmode;
+struct Menu M2offhour;
+struct Menu M3onhour;
+struct Menu M1setTime;
+struct Menu M2sethouram;
+struct Menu M2sethourpm;
+struct Menu M2setminam;
+struct Menu M2setminpm;
+struct Menu M1eventMode;
+struct Menu M1reset;
+struct Menu M1rainbow;
+
+struct Menu M0main={"Main","WHITE",7,0,{
+    {1,&M1mode},
+    {2,&M1cmode},
+    {3,&M1nightmode},
+    {4,&M1setTime},
+    {5,&M1eventMode},
+    {6,&M1reset},
+    {7,&M1rainbow}    
+}};
 struct Menu M1mode={"Mode","CYAN",5,setMode,{
     {1,0},//Hour only
     {2,0},//Hour + temp (slow)
@@ -157,7 +198,6 @@ struct Menu M1rainbow={"Rainbow!","WHITE",3,setRainbow,{
     {2,0},//Rainbow off
     {12,&M0main}//return
 }};
-struct Menu M0main={"Main","WHITE",8,0,{{1,&M1mode},{2,&M1cmode},{3,&M1nightmode},{4,&M1setMin},{5,&M1eventMode},{6,&M1reset},{7,&M1rainbow}}};
 
 int findNextEntry(struct Menu* m,int i){
     if((i+2)>m->nb_optn)return 0;
@@ -165,22 +205,24 @@ int findNextEntry(struct Menu* m,int i){
 }
 
 struct Menu* getSubMenu(struct Menu* m,int index){
-    if(m->sub[index].submenu==0)return &M0main;//if executed NULL, return to main menu
+    if(m->sub[index].submenu==0)return 0;//if executed NULL, return blank menu (==exit setup)
     return m->sub[index].submenu;
 }
 int main(void){
     char c=' ';
     int index=0;
-    struct Menu* Mcurrent=&M0main;
-    printf("%lu\r\n",sizeof(int));
-    printf("%lu\r\n",sizeof(Mcurrent));
+    struct Menu* Mcurrent=0;
     while(c!='q'){
         if(c!='\n'){
             if(c=='e'){
-                if(Mcurrent->fptr!=0 && Mcurrent->sub[index].submenu==0)Mcurrent->fptr(index);//check if function is associated and selected value is not a navigation move
-                Mcurrent=getSubMenu(Mcurrent,index);//get new menu
-                index=0;
-            }else if(c=='+'){
+                if(Mcurrent){
+                    if(Mcurrent->fptr!=0 && Mcurrent->sub[index].submenu==0)Mcurrent->fptr(index);//check if function is associated and selected value is not a navigation move
+                    Mcurrent=getSubMenu(Mcurrent,index);//get new menu
+                    index=0;
+                }else{
+                    Mcurrent=&M0main;
+                }
+            }else if(c=='+' && Mcurrent){
                 leds[Mcurrent->sub[index].led].on=0;
                 index=findNextEntry(Mcurrent,index);//get following option
                 leds[Mcurrent->sub[index].led].on=1;
@@ -188,7 +230,7 @@ int main(void){
             for(int i=0;i<19;i++){
                 if(leds[i].on)printf("Led :%s ",leds[i].name);
             }
-            printf("\r\nCurrent Menu :%s, pointing to %s\r\n",Mcurrent->name,Mcurrent->sub[index].submenu?Mcurrent->sub[index].submenu->name:"NULL");
+            if(Mcurrent)printf("\r\nCurrent Menu :%s, pointing to %s\r\n",Mcurrent->name,Mcurrent->sub[index].submenu?Mcurrent->sub[index].submenu->name:"NULL");
         }
         scanf("%c",&c);
     }
