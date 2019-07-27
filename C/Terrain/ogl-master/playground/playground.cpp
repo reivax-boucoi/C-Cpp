0ec8e2a573a34e5c9e12c97ce2101b99ccedb637
reivax-boucoi <reivax.boucoi@laposte.net>
2019-07-27 18:48:47 +0200

    a

--- C/Terrain/ogl-master/playground/playground.cpp
+++ C/Terrain/ogl-master/playground/playground.cpp
@@ -9,6 +9,8 @@ GLFWwindow* window;
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include <common/shader.hpp>
+
 int main( void )
 {
 	// Initialise GLFW
@@ -37,6 +39,7 @@ int main( void )
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -50,10 +53,49 @@ int main( void )
 	// Dark blue background
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
-	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
-		// Draw nothing, see you in tutorial 2 !
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+
+
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, -0.8f, 0.0f,
+		 1.0f, -0.8f, 0.0f,
+		 0.0f,  0.8f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	do{
+
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -62,6 +104,11 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
