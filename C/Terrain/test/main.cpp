b375326e8b73140759583dae26a9edf0b08d83f6
reivax-boucoi <reivax.boucoi@laposte.net>
2019-07-30 14:17:04 +0200

    a

--- C/Terrain/test/main.cpp
+++ C/Terrain/test/main.cpp
@@ -8,39 +8,41 @@ void setup() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     
 }
-/*
+
 void display(){
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glRectf(-0.75f,0.75f, 0.75f, -0.75f);
-      glutSwapBuffers();
-}*/
-void display() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
- 
-   // Draw a Red 1x1 Square centered at origin
-   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-      glColor3f(1.0f, 0.0f, 0.0f); // Red
-      glVertex2f(-0.5f, -0.5f);    // x, y
-      glVertex2f( 0.5f, -0.5f);
-      glVertex2f( 0.5f,  0.5f);
-      glVertex2f(-0.5f,  0.5f);
-   glEnd();
- 
-   glFlush();  // Render now
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex2f(-0.5f, -0.5f);    // x, y
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.5f,  0.5f);
+    glVertex2f(-0.5f,  0.5f);
+    glEnd();
+    
+    glutSwapBuffers();
+}
+void changeSize(int w, int h) {
+	if(h == 0)h = 1;
+	float ratio = 1.0* w / h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(45,ratio,1,1000);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char *argv[]){
     
-     glutInit(&argc, argv);
-     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-     glutInitWindowSize(640,480);
-     glutCreateWindow("Hello World");
-
-     setup();
-     glutDisplayFunc(display);
-     glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(640,480);
+    glutCreateWindow("Hello World");
     
-     return 0;
-  }
+    setup();
+    glutDisplayFunc(display);
+	//glutReshapeFunc(changeSize);
+    glutMainLoop();
+    
+    return 0;
+}
